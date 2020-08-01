d9d0fd32a19d12184861ed3f228761c6aff3868f
Carlyle Douglas <cdouglascorp@gmail.com>
2020-07-31 19:24:09 -0600

    Add test suite for User class

--- test/User-test.js
+++ test/User-test.js
@@ -4,9 +4,17 @@ const expect = chai.expect;
 const User = require('../src/User').default;
 
 describe('User', function() {
-  let user;
+  let user1, user2;
   beforeEach(function() {
-    user = new User()
+    user1 = new User({
+      id: 1,
+      name: "Leatha Ullrich"
+    })
+    user2 = new User({
+      id: 2,
+      name: "Rocio Schuster"
+    })
+
   });
 
   it('should be a function', function() {
@@ -14,6 +22,22 @@ describe('User', function() {
   });
 
   it('should be an instance of User', function(){
-    expect(user).to.be.an.instanceof(User)
+    expect(user1).to.be.an.instanceof(User)
+  });
+
+  it('should have an id', function() {
+    expect(user1.id).to.equal(1)
+  });
+
+  it('should be able to have a different id', function() {
+    expect(user2.id).to.equal(2)
+  });
+
+  it('should have a name', function() {
+    expect(user1.name).to.equal("Leatha Ullrich")
+  });
+
+  it('should be able to have a different name', function() {
+    expect(user2.name).to.equal("Rocio Schuster")
   });
 });
