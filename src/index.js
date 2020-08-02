ed872ac3601de93bf1d8c4bbcd27fe91de47b5f2
Carlyle Douglas <cdouglascorp@gmail.com>
2020-08-01 21:09:50 -0600

    Add fetch call for booking and room API

--- src/index.js
+++ src/index.js
@@ -4,8 +4,13 @@
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
+// An example of how you tell webpack to use an image (also need to link to
+// it in the index.html)
 import './images/turing-logo.png'
+
+let userData;
+let bookingData;
+let roomsData
 
 fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users")
   .then(response => response.json())
@@ -13,5 +18,16 @@ fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users")
     userData = data.users;
   }).catch(err => console.log('error: ', err))
 
+fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings")
+  .then(response => response.json())
+  .then(data => {
+    bookingData = data.bookings;
+  }).catch(err => console.log('error: ', err))
+
+fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms")
+  .then(response => response.json())
+  .then(data => {
+    roomsData = data.rooms;
+  }).catch(err => console.log('error: ', err))
 
 // console.log('This is the JavaScript entry file - your code begins here.');
