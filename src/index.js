ae80faa2adcb39c40200f997b8296c9fc81a06af
Carlyle Douglas <cdouglascorp@gmail.com>
2020-08-03 19:05:17 -0600

    Move fetch data to apiCalls.js

--- src/index.js
+++ src/index.js
@@ -7,25 +7,6 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to
 // it in the index.html)
 import './images/turing-logo.png'
-
-let userData;
-let bookingData;
-let roomsData
-
-fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users")
-  .then(response => response.json())
-  .then(data => {
-    userData = data.users;
-  }).catch(err => console.log('error: ', err))
-
-fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings")
-  .then(response => response.json())
-  .then(data => {
-    bookingData = data.bookings;
-  }).catch(err => console.log('error: ', err))
-
-fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms")
-  .then(response => response.json())
-  .then(data => {
-    roomsData = data.rooms;
-  }).catch(err => console.log('error: ', err))
+import './domUpdates.js'
+let userData, bookingData, roomsData;
+import './apiCalls.js'
