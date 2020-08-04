13e47a6ae46f20777b9e90bf77ac91f7c755bb5d
Carlyle Douglas <cdouglascorp@gmail.com>
2020-08-03 19:05:37 -0600

    Create new file to hold api calls

---
+++ src/apiCalls.js
@@ -0,0 +1,19 @@
+//API FETCH CALLS
+
+fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users")
+  .then(response => response.json())
+  .then(data => {
+    userData = data.users;
+  }).catch(err => console.log('error: ', err))
+
+fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings")
+  .then(response => response.json())
+  .then(data => {
+    bookingData = data.bookings;
+  }).catch(err => console.log('error: ', err))
+
+fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms")
+  .then(response => response.json())
+  .then(data => {
+    roomsData = data.rooms;
+  }).catch(err => console.log('error: ', err))
