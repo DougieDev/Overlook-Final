ceca2ab89835090b3ed34cf1eb473c82fd880f0d
Carlyle Douglas <cdouglascorp@gmail.com>
2020-08-04 17:19:24 -0600

    Add methods for Manager class

--- src/Manager.js
+++ src/Manager.js
@@ -4,6 +4,14 @@ class Manager {
     this.bookingData = bookingData
   }
 
+  getBookedRooms(date) {
+    let bookedRooms = this.bookingData.filter(booking => {
+       return booking.date === date
+    }).map(booking => {
+      return booking.roomNumber
+    })
+  }
+
   getRoomsAvailable(date) {
    let bookedRooms = this.bookingData.filter(booking => {
       return booking.date === date
@@ -15,5 +23,27 @@ class Manager {
    })
   }
 
+  getTotalRevenue(date) {
+    let bookedRooms = this.bookingData.filter(booking => {
+       return booking.date === date
+    }).map(booking => {
+      return booking.roomNumber
+    })
+    return this.roomsData.reduce((acc, room) => {
+      if (bookedRooms.includes(room.number)) {
+        acc += room.costPerNight
+      }
+      return acc;
+    }, 0)
+  }
+
+  getOccupancyPercentage(date) {
+    let bookedRooms = this.bookingData.filter(booking => {
+       return booking.date === date
+    }).map(booking => {
+      return booking.roomNumber
+    })
+    return (bookedRooms.length / this.roomsData.length) * 100
+  }
 }
 export default Manager;
