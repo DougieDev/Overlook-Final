d29ad0c8be3a398da5ecafa358b62dd77ad5f678
Carlyle Douglas <cdouglascorp@gmail.com>
2020-08-04 18:57:35 -0600

    Fix indentation

--- src/mockBookings.js
+++ src/mockBookings.js
@@ -1,25 +1,25 @@
 const mockBookings = [
   {
-        "id": "5fwrgu4i7k55hl6t8",
-        "userID": 1,
-        "date": "2020/02/05",
-        "roomNumber": 12,
-        "roomServiceCharges": []
-    },
-    {
-        "id": "5fwrgu4i7k55hl6t9",
-        "userID": 38,
-        "date": "2020/02/05",
-        "roomNumber": 14,
-        "roomServiceCharges": []
-    },
-    {
-        "id": "5fwrgu4i7k55hl6ta",
-        "userID": 25,
-        "date": "2020/01/11",
-        "roomNumber": 9,
-        "roomServiceCharges": []
-    }
-  ]
+    "id": "5fwrgu4i7k55hl6t8",
+    "userID": 1,
+    "date": "2020/02/05",
+    "roomNumber": 12,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6t9",
+    "userID": 38,
+    "date": "2020/02/05",
+    "roomNumber": 14,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6ta",
+    "userID": 25,
+    "date": "2020/01/11",
+    "roomNumber": 9,
+    "roomServiceCharges": []
+  }
+]
 
-    export default mockBookings;
+export default mockBookings;
