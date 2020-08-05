708ae329f1fa4e4f16a482d401bc9fab459b9851
Carlyle Douglas <cdouglascorp@gmail.com>
2020-08-04 18:38:40 -0600

    Add methods to get back room bookings for a specific date

--- src/User.js
+++ src/User.js
@@ -2,6 +2,16 @@ class User {
   constructor(userInfo) {
     this.id = userInfo.id
     this.name = userInfo.name
+    this.bookingData = userInfo.bookingData
+    this.roomsData = userInfo.roomsData
+  }
+
+  getUserRoomBookings(date) {
+    return this.bookingData.filter(booking => booking.date === date && booking.userID === this.id)
+  }
+
+  getTotalSpent(date) {
+
   }
 }
 
