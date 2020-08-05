7eb2ce364692ffa46c81aabe8c6d95ecaa173c18
Carlyle Douglas <cdouglascorp@gmail.com>
2020-08-04 18:58:34 -0600

    Add mock data for rooms

---
+++ src/mockRooms.js
@@ -0,0 +1,28 @@
+const mockRooms = [
+  {
+    "number": 12,
+    "roomType": "single room",
+    "bidet": false,
+    "bedSize": "twin",
+    "numBeds": 2,
+    "costPerNight": 172.09
+  },
+  {
+    "number": 13,
+    "roomType": "single room",
+    "bidet": false,
+    "bedSize": "queen",
+    "numBeds": 2,
+    "costPerNight": 423.92
+  },
+  {
+    "number": 14,
+    "roomType": "residential suite",
+    "bidet": false,
+    "bedSize": "twin",
+    "numBeds": 1,
+    "costPerNight": 457.88
+  }
+]
+
+export default mockRooms;
