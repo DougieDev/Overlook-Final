102e030297883e41452a7010d8e6a40148c7c08f
Carlyle Douglas <cdouglascorp@gmail.com>
2020-08-05 18:05:41 -0600

    Add function to create post

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -12,8 +12,14 @@ fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings")
     bookingData = data.bookings;
   }).catch(err => console.log('error: ', err))
 
+
 fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms")
   .then(response => response.json())
   .then(data => {
     roomsData = data.rooms;
   }).catch(err => console.log('error: ', err))
+
+// const createPost = (options) => {
+//   fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings", options)
+//     .then(response = response.json()
+// }
